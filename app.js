0d6642ee6ca2af832890a5d47dd44981b54ea1ca
ali.rafiq@bykea.com <ali.rafiq@bykea.com>
2021-06-13 13:10:12 +0500

    fix

---
+++ app.js
@@ -0,0 +1,33 @@
+'use strict';
+process.env.NODE_ENv = 'develop';
+
+require('./root-require');
+const Bb = require('bluebird');
+
+/* eslint-disable no-console */
+console.log(`Loading ${process.argv[2]}...`);
+
+if (!process.env.NODE_ENV) {
+  console.error('\x1b[31m',
+    'NODE_ENV is not defined! Using default development environment');
+  process.env.NODE_ENV = 'development';
+}
+console.log('\x1b[7m',
+  `Application loaded using the "${process.env.NODE_ENV}" environment configuration`);
+console.log('\x1b[0m');
+
+Bb.config({
+  longStackTraces: true,
+  warnings: {
+    wForgottenReturn: false,
+  },
+});
+
+process.on('unhandledRejection', (reason, p) => {
+  console.error('Unhandled Rejection at:', p, 'reason:', reason);
+});
+
+/* eslint-enable no-console */
+console.log(process.argv)
+// eslint-disable-next-line import/no-dynamic-require, global-require
+Promise.resolve(require(process.argv[2])());
